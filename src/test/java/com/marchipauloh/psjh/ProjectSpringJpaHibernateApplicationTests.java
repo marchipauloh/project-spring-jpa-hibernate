6f38f752ee5b7876e06ff2b58a479cefd47d4791
Paulo Marchi <marchipaulo@gmail.com>
2022-07-04 20:27:52 -0300

    Project created

---
+++ src/test/java/com/marchipauloh/psjh/ProjectSpringJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marchipauloh.psjh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
