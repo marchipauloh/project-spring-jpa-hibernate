a7ad7ecf025fb377f6b27d065bfd221540044e4b
Paulo Marchi <marchipaulo@gmail.com>
2022-07-05 16:59:42 -0300

    OrderStatus enum

--- src/main/java/com/marchipauloh/psjh/entities/Order.java
+++ src/main/java/com/marchipauloh/psjh/entities/Order.java
@@ -13,6 +13,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.marchipauloh.psjh.entities.enums.OrderStatus;
 
 @Entity
 @Table(name = "tb_order")
@@ -26,7 +27,9 @@ public class Order implements Serializable{
 	private Long id;
 	
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
-	private Instant instant;
+	private Instant moment;
+	
+	private Integer orderStatus;
 	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
@@ -36,10 +39,11 @@ public class Order implements Serializable{
 		
 	}
 
-	public Order(Long id, Instant instant, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
-		this.instant = instant;
+		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -52,11 +56,21 @@ public class Order implements Serializable{
 	}
 
 	public Instant getInstant() {
-		return instant;
+		return moment;
 	}
 
 	public void setInstant(Instant instant) {
-		this.instant = instant;
+		this.moment = instant;
+	}
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}		
 	}
 
 	public User getClient() {
@@ -82,7 +96,6 @@ public class Order implements Serializable{
 			return false;
 		Order other = (Order) obj;
 		return Objects.equals(id, other.id);
-	}
-	
+	}	
 	
 }
