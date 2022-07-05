2991020e98d834e0b2dc4a8a03f32cf3dd60b561
Paulo Marchi <marchipaulo@gmail.com>
2022-07-05 20:36:33 -0300

    Category

---
+++ src/main/java/com/marchipauloh/psjh/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.marchipauloh.psjh.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.marchipauloh.psjh.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
