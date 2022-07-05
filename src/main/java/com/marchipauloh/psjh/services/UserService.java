f1b9adb216a623e4e7df650ee2b42d1cea519911
Paulo Marchi <marchipaulo@gmail.com>
2022-07-04 23:17:05 -0300

    Service layer, component registration

---
+++ src/main/java/com/marchipauloh/psjh/services/UserService.java
@@ -0,0 +1,26 @@
+package com.marchipauloh.psjh.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marchipauloh.psjh.entities.User;
+import com.marchipauloh.psjh.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
